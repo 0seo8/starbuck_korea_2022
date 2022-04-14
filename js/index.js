2663ddafafc6785c2e9b21d83096ffee4b6512be
0seo8 <0seo23@gmail.com>
2022-04-14 15:50:31 +0900

    feat: Create header-design

---
+++ js/index.js
@@ -0,0 +1,19 @@
+import '../css/main.css'
+
+const $input = document.querySelector('.sub-menu input[type=text]');
+const $searchIcon = document.querySelector('.sub-menu .material-icons');
+
+$searchIcon.addEventListener('click', () => {
+  $input.focus()
+})
+
+$input.addEventListener('focus', () => {
+  $input.setAttribute('placeholder', '통합검색');
+  $searchIcon.style.visibility = 'hidden'
+})
+
+$input.addEventListener('blur', () => {
+  $input.setAttribute('placeholder', '');
+  $searchIcon.style.visibility = 'visible'
+})
+
---
+++ scss/READMD.md
@@ -0,0 +1,20 @@
+# Sass
+
+## Sass 7-1패턴
+
+- abstracts
+  - sass변수, sass믹스인, sass플레이스 홀더
+- base
+  - 리셋, 정규화, 타이포그래피 규칙
+- components
+  - 버튼, 캐러셀, 커버, 드롭다운, 기타
+- layout
+  - 네비게이션, 그리드 시스템, 헤더, 푸터, 사이드바, 폼, 기타
+- pasges
+  - 폼 한정스타일, 연락처 한정스타일, 기타
+- thems    
+  - 디폴트테마, 관리자테마, 기타
+- vendors
+  - bootstrap, JQueryUI, 기타  
+
+- main.scss
