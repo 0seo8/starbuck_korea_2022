d2b93e55163a815aef6aadccab8661c630f564c5
0seo8 <0seo23@gmail.com>
2022-04-14 16:38:29 +0900

    feat: Create Badge Area

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-import '../css/main.css'
-
 const $input = document.querySelector('.sub-menu input[type=text]');
 const $searchIcon = document.querySelector('.sub-menu .material-icons');
+const $badgeEl = document.querySelector('.badges');
+
 
 $searchIcon.addEventListener('click', () => {
   $input.focus()
@@ -17,3 +17,18 @@ $input.addEventListener('blur', () => {
   $searchIcon.style.visibility = 'visible'
 })
 
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if(window.scrollY > 500 ) {
+    //배지숨기기
+    gsap.to($badgeEl, .6, {
+      opacity: 0
+    });
+
+  } else {
+    //배지를 보여주기
+    gsap.to($badgeEl, .6, {
+      opacity: 1
+    });
+  }
+}, 300));
