8576affb054d3cf501e9454748db23d313b570f0
0seo8 <0seo23@gmail.com>
2022-04-15 22:29:38 +0900

    feat: Finish Page

--- js/index.js
+++ js/index.js
@@ -54,3 +54,15 @@ $promotionEls.forEach(function($promotionEl, index) {
     opacity: 1
   })
 })
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy') // section이라는 태그 중 scroll-spy가 붙은 요소들을 전부 찾겠다.
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
+})
